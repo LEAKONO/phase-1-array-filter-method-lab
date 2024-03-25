8feaa16b92af015e7c969a31e5bc87879db3b7f4
Emmanuel Leakono <leakonoemmanuel3@gmail.com>
2024-03-25 22:59:27 +0300

    Array filter method

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers, query) {
+    const lowerCaseQuery = query.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowerCaseQuery);
+}
+
+
+
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.startsWith(query));
+}
+
+
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
